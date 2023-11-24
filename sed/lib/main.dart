ce04f8e354dee5068875493d9423d045b6dc1bc2
Mateus Neves <mateus.neves@sou.inteli.edu.br>
2023-11-23 23:22:56 -0300

    Auto stash before merge of "main" and "origin/main"

--- sed/lib/main.dart
+++ sed/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:sed/criar_entrega/criar_entrega.dart';
+import 'package:sed/home/home.dart';
 import 'login/login.dart'; // Importe a sua nova página
 import 'listagem/listagem.dart'; // Importe a sua nova página
 import 'listagem_escolas/listagem_escolas.dart';
 import 'lista_compras/lista_compras.dart';
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +18,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: lista_compras(), // Use sua nova página aqui
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) =>  login_escola(),
+        '/home': (context) =>  const Home(),
+        '/listagem': (context) =>   listagem(),
+        '/cadastarEntrega': (context) =>   CadastroEntregaScreen(),
+        '/listagemEscolas': (context) =>   lista_escolas(),
+      },
     );
   }
 }
